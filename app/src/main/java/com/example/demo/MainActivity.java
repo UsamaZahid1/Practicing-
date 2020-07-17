61c2b1d3165509bddce8fefa4fb5ce8b8151c91c
Usama Zahid <uzahid231@gmail.com>
2020-07-17 13:06:02 +0500

    Second screen and activity.

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.demo;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
 
 
     }
