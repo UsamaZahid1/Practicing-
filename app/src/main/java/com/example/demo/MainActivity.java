52001d14e5943ec513c13b281e8c2d5ca09af0b2
Usama Zahid <uzahid231@gmail.com>
2020-07-17 11:20:06 +0500

    Practicing

---
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
