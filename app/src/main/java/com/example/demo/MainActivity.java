763062d9e4b41ba9ab9108ded4a9b0d44bf0595f
Usama Zahid <uzahid231@gmail.com>
2020-07-17 15:12:08 +0500

    button

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.demo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,13 +11,19 @@ public class MainActivity extends AppCompatActivity {
     Button btn;
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.mainActivityBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(i);
 
 
-
+            }
+        });
 
 
     }
